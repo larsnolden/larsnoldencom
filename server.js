644aed256543e350961251fade45045505493aef
Lars Nolden <larsnolden@web.de>
2018-06-02 19:51:49 -0400

    port is env variable now

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const Lars = require("./Lars");
 
-const port = 80;
+const port = process.env.PORT || 8080;
 const app = express();
 
 app.get('/', (req, res) => {
